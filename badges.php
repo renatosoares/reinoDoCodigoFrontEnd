c356ee31edb8fed18f30264be9bb8a9b95b3b08e
Renato Soares <renatosoarespro@hotmail.com>
2015-11-23 08:47:29 -0300

    janela compartilhar

--- badges.php
+++ badges.php
@@ -42,8 +42,8 @@
 								</button>
 							</div>
 							
-						</div>
-					</div>
+						</div> <!-- /caption -->
+					</div> <!-- /thumbnail -->
 				</div>
 
 				<?php endfor;  ?>
@@ -68,12 +68,65 @@
 	        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
 	      </div>
 	      <div class="modal-body">
-	        ...
-	      </div>
+	        <div class="row">
+	        	<div class="col-md-4 text-center">
+	        		<img src="images/smokin.png" alt="badge">
+					<h3>Lições Completas</h3>
+					<h4>6 Nov, 2015</h4>
+	        	</div>
+	        	<div class="col-md-1">&nbsp;</div>
+	        	<div class="col-md-7">
+	        		<table class="table table-hover table-condensed">
+		        		<thead>
+							<tr>
+								<th>Issuer Details</th>
+								<th>&nbsp;</th>
+							</tr>
+						</thead>
+						<tbody class="">
+							<tr>
+								<td> <strong> Nome </strong></td>
+								<td>Bob Esponja</td>
+							</tr>
+							<tr>
+								<td><strong>URL</strong></td> 
+								<td>http://www.reinodocodigo.org</td>
+							</tr>
+							<tr>
+								<td><strong>Organização</strong></td>
+								<td>Reino do Código</td>
+							</tr>
+						</tbody>
+					</table>	     
+					<table class="table table-hover table-condensed">
+		        		<thead>
+							<tr>
+								<th>Badge Details</th>
+								<th>&nbsp;</th>
+							</tr>
+						</thead>
+						<tbody class="">
+							<tr>
+								<td> <strong> Nome</strong></td>
+								<td> Smokin' Hot</td>
+							</tr>
+							<tr>
+								<td><strong>Descrição</strong></td> 
+								<td>bla bla bla</td>
+							</tr>
+							<tr>
+								<td><strong>Criteria</strong></td>
+								<td>Bla bla bla</td>
+							</tr>
+						</tbody>
+					</table>
+	        	</div>
+	        </div> <!-- /row -->
+	      </div> <!-- /modal-body -->
 	      <div class="modal-footer">
 	        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
 	        <button type="button" class="btn btn-primary">Save changes</button>
-	      </div>
-	    </div>
-	  </div>
+	      </div> <!-- /modal-footer -->
+	    </div> <!-- /modal-content -->
+	  </div> <!-- /modal-dialog -->
 	</div> <!-- /modal -->
